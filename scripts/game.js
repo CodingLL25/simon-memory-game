d943f9096ce81384e1e5c6bc6dc0af86730ce320
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-09-14 12:01:56 +0100

    Initial code along video

---
+++ scripts/game.js
@@ -0,0 +1,12 @@
+let game = {
+    currentGame: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"],
+    playerMoves: [],
+};
+
+
+module.exports = {
+    // curly braces used as more than one function to be exported
+    game
+};
